7b4db661f3084d2163c5a0a6d9901035cb2d103e
Tkelly8 <edenaun@gmail.com>
2023-08-19 21:29:35 +0000

    tested program
    Signed-off-by: Tkelly8 <edenaun@gmail.com>

--- main.cpp
+++ main.cpp
@@ -15,32 +15,36 @@ Location: 0x4353 Error Value: 0xFFFFFFFF
 
 
 DigitalOut myled(LED1);
-Serial pc(USBRX, USBTX);
-Serial dev(p9, p10);
+UnbufferedSerial  pc(USBTX, USBRX);  //using unbufferedserial as per mbed OS6
+UnbufferedSerial  dev(PB_6, PB_7);  // looked up pins in data sheet, these ones worked
 
 void dev_recv(){
+    char c;
     while(dev.readable()){
-        pc.putc(dev.getc())    
+        dev.read(&c, 1);
+        pc.write(&c, 1);   //putc and getc gave error so took this from example program on github
     }    
 }
 
 void pc_recv(){
+    char c;
     while(pc.readable()){
-        dev.putc(pc.getc());    
+        pc.read(&c, 1);
+        dev.write(&c, 1);
     }    
 }
 
 int main() {
     pc.baud(9600);
-    device1.baud(115200);
+    dev.baud(9600);   //device1 does not exist
     pc.attach(&pc_recv);
-    dev.atach(&dev);
-    pc.printf("Hello!! \r\n");
+    dev.attach(&dev_recv);
+    pc.write("\rHello!!\r\n",10);//  printf would't work so used write
     
     while(1) {
         myled = 1;
-        wait(1);
+        wait_us(1000000); //changed to micro seconds
         myled = 0;
-        wait(1);
+        wait_us(1000000);
     }
-}
+}   // no run time error showed up
